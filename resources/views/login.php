92f6a80ab159cc68b1c00e7891dd107bd497a883
TIM-JUS <1440406@senati.pe>
2023-10-11 13:28:56 -0500

    Validar los datos del usuario para que ingrese

--- resources/views/login.php
+++ resources/views/login.php
@@ -16,7 +16,31 @@ if (isset($_SESSION["user"])) {
     
 </head>
 <body>
-
+    
+    <div class="container">
+    <center><h2><b><color="red">INICIA SESION PARA VER LUGARES TURSITICOS DE HUANUCO</b></h1></center>
+        <?php
+        if (isset($_POST["login"])) {
+           $email = $_POST["email"];
+           $password = $_POST["password"];
+            require_once "database.php";
+            $sql = "SELECT * FROM users WHERE email = '$email'";
+            $result = mysqli_query($conn, $sql);
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if ($user) {
+                if (password_verify($password, $user["password"])) {
+                    session_start();
+                    $_SESSION["user"] = "yes";
+                    header("Location: index.php");
+                    die();
+                }else{
+                    echo "<div class='alert alert-danger'>Las contraseñas no coinciden</div>";
+                }
+            }else{
+                echo "<div class='alert alert-danger'>El email no coincide</div>";
+            }
+        }
+        ?>
       <form action="login.php" method="post">
         <div class="form-group">
             <b>Ingrese su correo</b>
