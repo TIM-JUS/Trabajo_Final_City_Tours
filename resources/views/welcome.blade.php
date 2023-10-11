9b3ed17b46cf197f7498915a27cc575fbdc9a2c4
TIM-JUS <1440406@senati.pe>
2023-10-11 13:22:56 -0500

    Estructura del html botones de entradas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,57 +21,7 @@ if (isset($_SESSION["user"])) {
     <div class="container">
         
     <center><h2><b>REGISTRES PARA VER LUGARES TURISTICOS DE HUANUCO</b></h1></center>
-        <?php
-        if (isset($_POST["submit"])) {
-           $fullName = $_POST["fullname"];
-           $email = $_POST["email"];
-           $password = $_POST["password"];
-           $passwordRepeat = $_POST["repeat_password"];
-           
-           $passwordHash = password_hash($password, PASSWORD_DEFAULT);
-
-           $errors = array();
-           
-           if (empty($fullName) OR empty($email) OR empty($password) OR empty($passwordRepeat)) {
-            array_push($errors,"Todos los campos son obligatorios");
-           }
-           if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            array_push($errors, "El correo no es válido");
-           }
-           if (strlen($password)<2) {
-            array_push($errors,"La contraseña debe tener al menos 3 caracteres");
-           }
-           if ($password!==$passwordRepeat) {
-            array_push($errors,"Las contraseñas no coiciden");
-           }
-           require_once "database.php";
-           $sql = "SELECT * FROM users WHERE email = '$email'";
-           $result = mysqli_query($conn, $sql);
-           $rowCount = mysqli_num_rows($result);
-           if ($rowCount>0) {
-            array_push($errors,"El Email ya existe!");
-           }
-           if (count($errors)>0) {
-            foreach ($errors as  $error) {
-                echo "<div class='alert alert-danger'>$error</div>";
-            }
-           }else{
-            
-            $sql = "INSERT INTO users (full_name, email, password) VALUES ( ?, ?, ? )";
-            $stmt = mysqli_stmt_init($conn);
-            $prepareStmt = mysqli_stmt_prepare($stmt,$sql);
-            if ($prepareStmt) {
-                mysqli_stmt_bind_param($stmt,"sss",$fullName, $email, $passwordHash);
-                mysqli_stmt_execute($stmt);
-                echo "<div class='alert alert-success'>Estás registrado con éxito.</div>";
-            }else{
-                die("Algo salió mal");
-            }
-           }
-          
-
-        }
-        ?>
+    
         <form action="welcome.blade.php" method="post">
             <div class="form-group">
             <b>Nombre</b>
@@ -101,24 +51,3 @@ if (isset($_SESSION["user"])) {
 </body>
 </html>
 
-<style>
-body{
-    padding:50px;
-}
-.container{
-    max-width: 600px;
-    margin:0 auto;
-    padding:50px;
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-}
-.form-group{
-    margin-bottom:30px;
-}
-
-h2{
-    color: red;
-}
-i{
-    color:green;
-}
-</style>
